45ca8062a9fa3c325daa0261b83e56186e3654a6
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2023-04-17 23:44:55 +0300

    Fixed: Made the topic suggestions prettier.

--- frontend/src/components/TopicSubscription.js
+++ frontend/src/components/TopicSubscription.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import api from "../services/api";
 import Autosuggest from "react-autosuggest";
+import "./TopicSubscription.css"; // Import the new CSS file
 
 const TopicSubscription = () => {
   const [topics, setTopics] = useState([]);
